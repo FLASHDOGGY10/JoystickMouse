501364254c55b632ce9fe0a95004d351c5712289
FLASHDOGGY10 <callum.r.mcrae@protonmail.com>
2025-12-11 15:12:36 +0000

    Added calibration button, added better notation

--- JoyStickMouse.ino
+++ JoyStickMouse.ino
@@ -1,79 +1,73 @@
 #include <ezButton.h>
 #include <BleMouse.h>
 
-BleMouse bleMouse;
+BleMouse bleMouse; //makes this a bleMouse
 
-// ---- ADC PINS ----
+// joystick varibles
 #define VRX_PIN  32
 #define VRY_PIN  33
 #define SW_PIN   18
 
-ezButton button(SW_PIN);
+//button decloration
+#define CB_PIN 4
 
+ezButton jsButton(SW_PIN);
+ezButton cbButton(CB_PIN);
 
-// ---- JOYSTICK CALIBRATION ----
+//joystick maths stuff
 int xCentre = 1771;
 int yCentre = 1840;
-
-const int deadZone = 120;       // small dead zone
+const int deadZone = 120;       
 const int maxAnalog = 4095;
-
-// speed multiplier
 int sensitivity = 3;
-
 int valueX, valueY;
 int mouseSpeedX, mouseSpeedY;
 
 void setup() {
-  BLEDevice::init("Joystick Mouse");
-
+  BLEDevice::init("Joystick Mouse"); // this makes the name 
   Serial.begin(115200);
   Serial.println("Starting BLE...");
-
   bleMouse.begin();
 
-  analogSetAttenuation(ADC_11db);
-  button.setDebounceTime(50);
+  analogSetAttenuation(ADC_11db); // does something, i dont really know
+
+  //button debounce 
+  jsButton.setDebounceTime(50);
+  cbButton.setDebounceTime(50);
 }
 
 void loop() {
-  button.loop();
+  //button loops
+  jsButton.loop();
+  cbButton.loop();
 
-
+  //joystick finding varibles 
   valueX = analogRead(VRX_PIN);
   valueY = analogRead(VRY_PIN);
-
   int xFromCentre = valueX - xCentre;
   int yFromCentre = valueY - yCentre;
-
-
   if (abs(xFromCentre) < deadZone) xFromCentre = 0;
   if (abs(yFromCentre) < deadZone) yFromCentre = 0;
-
-
   mouseSpeedX = map(xFromCentre, -2000, 2000, -7, 7);
-  mouseSpeedY = map(yFromCentre, -2000, 2000, 7, -7);  // invert Y for mouse
-
-
+  mouseSpeedY = map(yFromCentre, -2000, 2000, 7, -7);  
   mouseSpeedX = constrain(mouseSpeedX, -7, 7);
   mouseSpeedY = constrain(mouseSpeedY, -7, 7);
 
 
   if (bleMouse.isConnected()) {
-
-    if (button.isPressed()) {
-      Serial.println("Button pressed");
+  // big long if button pressed chain 
+    if (jsButton.isPressed()) {
       bleMouse.click(MOUSE_LEFT);
+    } else if (cbButton.isPressed()) 
+    {
+      calibrate();
     }
-
-
     if (mouseSpeedX != 0 || mouseSpeedY != 0) {
-      bleMouse.move(mouseSpeedX * sensitivity,
-                    -mouseSpeedY * sensitivity);
+      bleMouse.move(mouseSpeedX * sensitivity, -mouseSpeedY * sensitivity);
     }
   }
 
-
+  //lots for debugging
   Serial.print("X:");
   Serial.print(valueX);
   Serial.print(" Y:"); 
@@ -86,13 +80,13 @@ void loop() {
   delay(10);
 }
 
-void calibrate() 
+void calibrate()
 {
   Serial.println("return mouse to centre");
   delay(1000);
   xCentre = analogRead(VRX_PIN);
   yCentre = analogRead(VRY_PIN);
-
+  //centres mouse
   println("new X centre: " + xCentre);
   println("new Y centre: " + yCentre);
   
